84bd60ac086a49d1c9f3aa07287453bed08e77ff
SAXTEN2011 <amaxpierce@yahoo.com>
2018-10-06 14:31:01 -0500

    removed node server.js from piInstall sh

--- piInstall.sh
+++ piInstall.sh
@@ -1,4 +1,4 @@
 wget https://raw.githubusercontent.com/SAXTEN2011/WallabyFileWatcher/master/wallabyWatched.js &&
 wget https://raw.githubusercontent.com/SAXTEN2011/WallabyFileWatcher/master/package.json &&
 npm install &&
-node wallabyWatched.js
+echo to start: node wallabyWatched.js
