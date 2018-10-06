f04621b3abeb8b09f9b6b443b6c66949ab61703f
SAXTEN2011 <amaxpierce@yahoo.com>
2018-10-06 14:40:06 -0500

    added color to piinstall.sh

--- piInstall.sh
+++ piInstall.sh
@@ -1,4 +1,4 @@
 wget https://raw.githubusercontent.com/SAXTEN2011/WallabyFileWatcher/master/wallabyWatched.js &&
 wget https://raw.githubusercontent.com/SAXTEN2011/WallabyFileWatcher/master/package.json &&
 npm install &&
-echo to start: node wallabyWatched.js
+echo -e "\e[92m to start: node wallabyWatched.js"
